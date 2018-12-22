9ac329314f5b0429799d130b685190351b9e0707
Michael Moghaddam <m.moghaddam385@outlook.com>
2018-12-22 15:27:52 -0800

    Remove shared prefs until I can figure out wtf is wrong w/ iOS

--- groupme_bots/lib/cache/GroupMeMessageCache.dart
+++ groupme_bots/lib/cache/GroupMeMessageCache.dart
@@ -1,6 +1,4 @@
 
-import 'package:shared_preferences/shared_preferences.dart';
-
 final GroupMeMessageCache groupMeMessageCache = GroupMeMessageCache._privateCtor();
 
 class GroupMeMessageCache {
@@ -13,20 +11,11 @@ class GroupMeMessageCache {
   String get botId => _botId;
   set botId(String id) {
     _botId = id;
-    SharedPreferences.getInstance().then(
-      (prefs) => prefs.setString(_BOT_ID_PREFS_KEY, id)
-    ).catchError(
-      (error) => print("Error saving bot ID to prefs: $error")
-    );
   }
 
   // Get the initial bot ID from shared prefs 
   void initBotId() {
-    SharedPreferences.getInstance().then(
-      (prefs) => _botId = prefs.getString(_BOT_ID_PREFS_KEY)
-    ).catchError(
-      (error) => print("Error retrieving bot id from prefs: $error")
-    );
+    _botId = "placeholder";
   }
 
 }
